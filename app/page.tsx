aa4fec9ced0bd705169dd551fd19d1eefe43c1c9
gavrilenko_an <havrilenko.ann@gmail.com>
2025-11-26 15:06:28 +0200

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { redirect } from "next/navigation";
+import Image from "next/image";
+import styles from "./HomePage.module.scss";
 
 function Page() {
-  // remove
-  void redirect("/characters");
-  return null;
+  return(
+  <div className={styles.home}>
+      <div className={styles.content}>
+        <h1>Welcome to the Rick & Morty Explorer!</h1>
+
+        <div className={styles.gifWrapper}>
+          <Image
+            src="/images/rick-morty.gif"
+            alt="Rick & Morty GIF"
+            width={400}
+            height={400}
+          />
+        </div>
+
+        <p>Discover all your favorite characters, episodes, and locations.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
